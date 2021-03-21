38404117a21181b02031f9041d60fd5f2b9ae83a
RarceD <akimbo170@gmail.com>
2021-03-21 17:54:48 +0100

    create the backend on C#

---
+++ BackEnd/netcoreBackend/netcoreBackend/Controllers/ClassesController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using netcoreBackend.Models;
+
+namespace netcoreBackend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")] // Class controller ; -> classes/##
+    public class ClassesController : Controller
+    {
+
+        [HttpGet("")]
+        public ActionResult<string> GetAllGroups()
+        {
+            List<ChildClass> array_classes = new List<ChildClass>();
+            for (int i = 0; i<10; i++)
+            {
+                var a = new ChildClass();
+                a.icon = "hola";
+                a.id = i;
+                a.name = "name the mierda";
+                a.password = "passwords";
+                array_classes.Add(a);
+            }
+
+             return Ok(array_classes); 
+        }
+
+        // GET: ClassController
+        public ActionResult Index()
+        {
+             
+            return Ok("todo bien");
+        }
+
+        // GET: ClassController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ClassController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ClassController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ClassController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ClassController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ClassController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ClassController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ BackEnd/netcoreBackend/netcoreBackend/Models/ChildClass.cs
@@ -0,0 +1,12 @@
+﻿namespace netcoreBackend.Models
+{
+    public class ChildClass
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string password{ get; set; }
+        public string image { get; set; }
+        public string icon { get; set; }
+    }
+}
+
